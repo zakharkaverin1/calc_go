ed2f3945de4114416adc27aed95e388eaee8cbe9
zakharkaverin1 <104697415+zakharkaverin1@users.noreply.github.com>
2024-12-18 19:48:28 +0300

    Update calculation.go

--- calculation.go
+++ calculation.go
@@ -8,13 +8,6 @@ import (
 	"strings"
 )
 
-var (
-	ErrDivisionByZero      = errors.New("division by zero")
-	ErrInvalidExpression   = errors.New("invalid expression")
-	ErrSomethingWentWrong  = errors.New("something went wrong")
-	ErrExpressionNotString = errors.New("expression not a string")
-)
-
 func Calc(expression string) (float64, error) {
 	defer func() {
 		if r := recover(); r != nil {
